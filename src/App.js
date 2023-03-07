716d834eb8c7ff51cb6ddc231ca56175f6665673
stefanmicunovic <stefanmicunovic1992@gmail.com>
2023-03-07 02:18:15 +0100

    set page404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreatePlayer from "./components/create-player/CreatePlayer.jsx";
 import Game from "./components/game/Game.jsx";
+import Page404 from "./components/page-404/Page404";
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
         <Routes>
           <Route path={"/"} element={<CreatePlayer/>}></Route>
           <Route path={"/game"} element={<Game/>}></Route>
-          {/* <Route path={"/*"} element={<div></div>}></Route> */}
+          <Route path={"/*"} element={<Page404/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/page-404/Page404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style/Page404.css'
+
+function Page404() {
+  return (
+    <section id='page404'>
+        <img src="/img/error404.gif" alt="error404" />
+    </section>
+  )
+}
+
+export default Page404
